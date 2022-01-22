2192352a31d0465ce4ad2b7616e429138bedecd3
Phúc HP <dhphucvo.mkd@gmail.com>
2022-01-22 20:30:09 +0700

    New Feedback V.1 - Fix some bug User - Validation Phone number

--- modules/User/Requests/UserRequest.php
+++ modules/User/Requests/UserRequest.php
@@ -4,13 +4,13 @@ namespace Modules\User\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserRequest extends FormRequest {
+class UserRequest extends FormRequest{
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize() {
+    public function authorize(){
         return true;
     }
 
@@ -19,15 +19,15 @@ class UserRequest extends FormRequest {
      *
      * @return array
      */
-    public function rules() {
+    public function rules(){
         $method = segmentUrl(2);
-        switch ($method) {
+        switch($method){
             default:
                 return [
                     'name'     => 'required',
                     'username' => 'required|unique:users',
                     'email'    => 'required|email|unique:users',
-                    'phone'    => 'required|digits:8|unique:users',
+                    'phone'    => 'required|digits:10|unique:users',
                     'status'   => 'required',
                     'role_id'  => 'required',
                     'password' => 'required|min:6|confirmed',
@@ -38,7 +38,7 @@ class UserRequest extends FormRequest {
                     'name'     => 'required',
                     'username' => 'required|unique:users,username,' . $this->id,
                     'email'    => 'required|email|unique:users,email,' . $this->id,
-                    'phone'    => 'required|digits:8|unique:users,phone,' . $this->id,
+                    'phone'    => 'required|digits:10|unique:users,phone,' . $this->id,
                     'status'   => 'required',
                     'role_id'  => 'required',
                     'password' => 'nullable|min:6|confirmed',
@@ -47,18 +47,19 @@ class UserRequest extends FormRequest {
         }
     }
 
-    public function messages() {
+    public function messages(){
         return [
             'required'         => ':attribute' . trans(' can not be empty.'),
             'unique'           => ':attribute' . trans(' was exist.'),
             'confirmed'        => ':attribute' . trans(' confirmation does not match.'),
             'email'            => ':attribute' . trans(' must be a valid email address.'),
             'min'              => ':attribute' . trans(' too short.'),
+            'digits'           => ':attribute' . trans(' must have 10 digits.'),
             'role_id.required' => trans('Please select ') . ':attribute'
         ];
     }
 
-    public function attributes() {
+    public function attributes(){
         return [
             'name'     => trans('Name'),
             'username' => trans('Username'),
